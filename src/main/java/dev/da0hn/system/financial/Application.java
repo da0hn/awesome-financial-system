b36c7028bdc121fedadace8fa67f78ba04b56bfd
Gabriel Honda <gabrielhondacba@gmail.com>
2024-07-17 13:55:05 -0400

    chore: initial commit

---
+++ src/main/java/dev/da0hn/system/financial/Application.java
@@ -0,0 +1,13 @@
+package dev.da0hn.system.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinancialSystemApplication.class, args);
+  }
+
+}
